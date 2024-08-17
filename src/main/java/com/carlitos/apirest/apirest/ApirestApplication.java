aeabf8784085a4a79f7c82543ce345ec024eaf21
unknown <u20231213568@usco.edu.co>
2024-08-16 21:21:28 -0500

    Fix: not finding database, some project setting changed

--- src/main/java/com/carlitos/apirest/apirest/ApirestApplication.java
+++ src/main/java/com/carlitos/apirest/apirest/ApirestApplication.java
@@ -3,11 +3,17 @@ package com.carlitos.apirest.apirest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ApirestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApirestApplication.class, args);
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        System.setProperty("SPRING_DATASOURCE_DB", dotenv.get("SPRING_DATASOURCE_DB"));
+        SpringApplication.run(ApirestApplication.class, args);
 	}
-
 }
